aa4e4b8076ce8f41f4a729e3ee7eb9692771c294
Melvin van Megen <melvin.vmegen@gmail.com>
2020-08-17 15:24:31 +0200

    vuetify added

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import wb from './registerServiceWorker'
+import vuetify from './plugins/vuetify'
 
 Vue.prototype.$workbox = wb
 Vue.config.productionTip = false
 
 new Vue({
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
